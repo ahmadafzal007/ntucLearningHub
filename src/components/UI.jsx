30384751dfb555a994871f16cb4700ab584f45d7
Ahmad <ahmadafzalch007@gmail.com>
2024-07-14 16:15:55 +0500

    UI updated

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -14,14 +14,17 @@ export const UI = ({ hidden, ...props }) => {
 
   const startListening = () => {
     if (!recognition) {
-      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
       const recognitionInstance = new SpeechRecognition();
       recognitionInstance.continuous = false;
       recognitionInstance.interimResults = false;
       recognitionInstance.lang = "en-US";
 
       recognitionInstance.onstart = () => {
-        console.log("Voice recognition started. Try speaking into the microphone.");
+        console.log(
+          "Voice recognition started. Try speaking into the microphone."
+        );
         setIsListening(true);
       };
 
@@ -108,28 +111,115 @@ export const UI = ({ hidden, ...props }) => {
           />
           <button
             onClick={sendMessage}
-            className="bg-blue-500 hover:bg-blue-600 text-white p-4 px-10 font-semibold uppercase rounded-md"
+            className="bg-blue-500 hover:bg-blue-600 text-white p-4 px-5 font-semibold uppercase rounded-md"
           >
             Send
           </button>
           <button
             onClick={startListening}
-            className={`pointer-events-auto bg-blue-500 hover:bg-blue-600 text-white p-4 rounded-md ${isListening ? "listening" : ""}`}
+            className={`pointer-events-auto bg-blue-500 hover:bg-blue-600 text-white p-4 rounded-md ${
+              isListening ? "bg-blue-700" : ""
+            }`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v9m0 0a3 3 0 006 0m-6 0a3 3 0 01-6 0m6 0V21m-6-3h12"
-              />
-            </svg>
+            {isListening ? (
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="-5 0 32 32"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
+                fill="#000000"
+                stroke="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>microphone</title>{" "}
+                  <desc>Created with Sketch Beta.</desc>{" "}
+                  <defs> </defs>{" "}
+                  <g
+                    id="Page-1"
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
+                    sketch:type="MSPage"
+                  >
+                    {" "}
+                    <g
+                      id="Icon-Set-Filled"
+                      sketch:type="MSLayerGroup"
+                      transform="translate(-107.000000, -309.000000)"
+                      fill="#ffffff"
+                    >
+                      {" "}
+                      <path
+                        d="M118,333 C121.866,333 125,329.866 125,326 L125,316 C125,312.134 121.866,309 118,309 C114.134,309 111,312.134 111,316 L111,326 C111,329.866 114.134,333 118,333 L118,333 Z M129,328 L127,328 C126.089,332.007 122.282,335 118,335 C113.718,335 109.911,332.007 109,328 L107,328 C107.883,332.799 112.063,336.51 117,336.955 L117,339 L116,339 C115.448,339 115,339.448 115,340 C115,340.553 115.448,341 116,341 L120,341 C120.552,341 121,340.553 121,340 C121,339.448 120.552,339 120,339 L119,339 L119,336.955 C123.937,336.51 128.117,332.799 129,328 L129,328 Z"
+                        id="microphone"
+                        sketch:type="MSShapeGroup"
+                      >
+                        {" "}
+                      </path>{" "}
+                    </g>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            ) : (
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="-3.5 0 32 32"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>microphone-off</title>{" "}
+                  <desc>Created with Sketch Beta.</desc> <defs> </defs>{" "}
+                  <g
+                    id="Page-1"
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
+                    sketch:type="MSPage"
+                  >
+                    {" "}
+                    <g
+                      id="Icon-Set-Filled"
+                      sketch:type="MSLayerGroup"
+                      transform="translate(-156.000000, -309.000000)"
+                      fill="#ffffff"
+                    >
+                      {" "}
+                      <path
+                        d="M169,335 C167.061,335 165.236,334.362 163.716,333.318 L162.31,334.742 C163.944,335.953 165.892,336.765 168,336.955 L168,339 L167,339 C166.448,339 166,339.448 166,340 C166,340.553 166.448,341 167,341 L171,341 C171.552,341 172,340.553 172,340 C172,339.448 171.552,339 171,339 L170,339 L170,336.955 C174.938,336.51 179.117,332.799 180,328 L178,328 C177.089,332.007 173.282,335 169,335 L169,335 Z M176,326 L176,320.739 L164.735,331.515 C165.918,332.432 167.386,333 169,333 C172.866,333 176,329.866 176,326 L176,326 Z M160.047,328.145 L160,328 L158,328 C158.109,328.596 158.271,329.175 158.478,329.733 L160.047,328.145 L160.047,328.145 Z M179.577,312.013 L155.99,334.597 L157.418,336.005 L181.014,313.433 L179.577,312.013 L179.577,312.013 Z M169,309 C165.134,309 162,312.134 162,316 L161.997,326.309 L175.489,313.401 C174.456,310.825 171.946,309 169,309 L169,309 Z"
+                        id="microphone-off"
+                        sketch:type="MSShapeGroup"
+                      >
+                        {" "}
+                      </path>{" "}
+                    </g>{" "}
+                  </g>{" "}
+                </g>{" "}
+              </svg>
+            )}
           </button>
         </div>
       </div>
